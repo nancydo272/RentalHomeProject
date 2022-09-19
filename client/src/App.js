fadfd6e019e065dd0840751fdac820c074fe7f14
Nancy Do <nancydo272@gmail.com>
2022-09-19 19:27:01 -0400

    Add.js Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route}  from 'react-router-dom';
+import AddRental from './components/AddRental'; 
+import Dashboard from './components/Dashboard'; 
+import EditRental from './components/EditRental'; 
+import Login from './components/Login'; 
+import Register from './components/Register'; 
+import ViewRental from './components/ViewRental'; 
+// import Map from './components/Map'; --> if we have time
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path ="/user/:id/dashboard" element ={<Dashboard /> } />
+          <Route path="/rental/:id" element ={<ViewRental />}/>
+          <Route path ="/addRental" element={<AddRental />} />
+          <Route path="/editRental/:id" element ={<EditRental />}/>
+          <Route path ="/register" element={<Register />} />
+          <Route path ="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
