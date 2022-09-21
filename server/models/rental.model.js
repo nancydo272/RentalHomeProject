46cb2bd6c88cbc3577314c3fea9601420a11219b
Nancy Do <nancydo272@gmail.com>
2022-09-20 20:47:37 -0400

    rental forms added

--- server/models/rental.model.js
+++ server/models/rental.model.js
@@ -64,3 +64,4 @@ const Rental = mongoose.model('Rental', RentalSchema);
 module.exports = Rental;
 
 //need to export so that we can use it in our frontend 
+//location is for the longtitude and latitude for google maps api
